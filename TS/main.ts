17c17a7b620c7cb02ad1d9fbfd4122ed183174e9
Faaris Mohamed <mohamed8876@students.cptc.edu>
2022-10-28 11:57:25 -0700

    Starting linking TS to JS

---
+++ TS/main.ts
@@ -0,0 +1,9 @@
+window.onload = function() {
+    let addBtn = 
+    <HTMLElement>document.querySelector("input[type=button]");
+    addBtn.onclick = addVideoGame;
+}
+
+function addVideoGame() {
+    alert("test");
+}
---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    var addBtn = document.querySelector("input[type=button]");
+    addBtn.onclick = addVideoGame;
+};
+function addVideoGame() {
+    console.log("Add video game was called");
+    
+}
