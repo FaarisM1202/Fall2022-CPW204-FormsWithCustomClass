c2ca1171e607827ca740be12f3b5a60cfcbe8611
Faaris Mohamed <mohamed8876@students.cptc.edu>
2022-10-28 14:00:41 -0700

    Created a game and grabbed data off the form

--- TS/main.ts
+++ TS/main.ts
@@ -1,3 +1,17 @@
+class VideoGame {
+    title:string;
+    price:number;
+    rating:string;
+    isDigitalOnly:boolean;
+}
+
+// test code
+// let myGame = new VideoGame();
+// myGame.title = "Mario";
+// myGame.price = 8.99;
+// myGame.rating = "E";
+// myGame.isDigitalOnly = true;
+
 window.onload = function() {
     let addBtn = 
     <HTMLElement>document.querySelector("input[type=button]");
@@ -5,5 +19,50 @@ window.onload = function() {
 }
 
 function addVideoGame() {
-    alert("test");
+    console.log("Add video game was called");
+
+     if((isAllDataValid)) {
+         let game = getVideoGame();
+         displayGame(game);
+     }
+}
+
+function $(id:string) {
+    return document.getElementById(id);
+}
+
+function getVideoGame():VideoGame {
+    // Create game
+    let game = new VideoGame();
+    // Get all data on the form and cast the element
+    let inputTitle = <HTMLInputElement>$("title");
+    game.title = inputTitle.value;
+
+    let priceInput = <HTMLInputElement>$("price");
+    // since the keyword value is being used here again, you'll have to
+    // use it differently.
+    game.price = parseFloat(priceInput.value);
+
+    let ratingInput = <HTMLSelectElement>$("rating");
+    game.rating = ratingInput.value;
+
+    let digitalOnly = <HTMLInputElement>$("online");
+    if(digitalOnly.checked) {
+        game.isDigitalOnly = true;
+    }
+    else {
+        game.isDigitalOnly = false;
+    }
+    // Return game
+
+}
+
+function displayGame(myGame:VideoGame):void {
+    // Display video game below the form
+
+}
+
+// Add validation code!
+function isAllDataValid() {
+    return true;
 }
--- js/main.js
+++ js/main.js
@@ -1,8 +1,40 @@
+var VideoGame = (function () {
+    function VideoGame() {
+    }
+    return VideoGame;
+}());
 window.onload = function () {
     var addBtn = document.querySelector("input[type=button]");
     addBtn.onclick = addVideoGame;
 };
 function addVideoGame() {
     console.log("Add video game was called");
-    
+    if ((isAllDataValid)) {
+        var game = getVideoGame();
+        displayGame(game);
+    }
+}
+function $(id) {
+    return document.getElementById(id);
+}
+function getVideoGame() {
+    var game = new VideoGame();
+    var inputTitle = $("title");
+    game.title = inputTitle.value;
+    var priceInput = $("price");
+    game.price = parseFloat(priceInput.value);
+    var ratingInput = $("rating");
+    game.rating = ratingInput.value;
+    var digitalOnly = $("online");
+    if (digitalOnly.checked) {
+        game.isDigitalOnly = true;
+    }
+    else {
+        game.isDigitalOnly = false;
+    }
+}
+function displayGame(myGame) {
+}
+function isAllDataValid() {
+    return true;
 }
